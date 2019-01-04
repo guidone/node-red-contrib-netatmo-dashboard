0f9e0b381f55a9b94f019ed4ebe036b913b8d1f2
Lyn Matten <matten@lm-consult.com>
2019-01-04 22:30:35 +0100

    fixed bug where values could be N.N. instead of 0

--- netatmo-dashboard.js
+++ netatmo-dashboard.js
@@ -41,8 +41,8 @@ module.exports = function (RED) {
                 _(data).each(function(station) {
                     if (station.type === 'NAMain') {
 
-                        console.log("\n\n\nNAMain: ", station);
-                        console.log("\n\n\ndashboard_data: ", station.dashboard_data);
+                        //console.log("\n\n\nNAMain: ", station);
+                        //console.log("\n\n\ndashboard_data: ", station.dashboard_data);
 
 
                         msg.payload.compact.reachable = station.reachable || "false";
@@ -50,13 +50,13 @@ module.exports = function (RED) {
                         msg.payload.compact.last_status_store = station.last_status_store;
 
                         if(typeof station.dashboard_data !== "undefined") {
-                            msg.payload.compact.temperature = station.dashboard_data.Temperature || "N.N.";
-                            msg.payload.compact.temperatureTrend = station.dashboard_data.temp_trend || "N.N.";
-                            msg.payload.compact.co2 = station.dashboard_data.CO2 || "N.N.";
-                            msg.payload.compact.humidity = station.dashboard_data.Humidity || "N.N.";
-                            msg.payload.compact.noise = station.dashboard_data.Noise || "N.N.";
-                            msg.payload.compact.pressure = station.dashboard_data.Pressure || "N.N.";
-                            msg.payload.compact.pressureTrend = station.dashboard_data.pressure_trend || "N.N.";
+                            msg.payload.compact.temperature = station.dashboard_data.Temperature !== "undefined" ? station.dashboard_data.Temperature : "N.N.";
+                            msg.payload.compact.temperatureTrend = station.dashboard_data.temp_trend !== "undefined" ? station.dashboard_data.temp_trend : "N.N.";
+                            msg.payload.compact.co2 = station.dashboard_data.CO2 !== "undefined" ? station.dashboard_data.CO2 : "N.N.";
+                            msg.payload.compact.humidity = station.dashboard_data.Humidity !== "undefined" ? station.dashboard_data.Humidity :  "N.N.";
+                            msg.payload.compact.noise = station.dashboard_data.Noise !== "undefined" ? station.dashboard_data.Noise : "N.N.";
+                            msg.payload.compact.pressure = station.dashboard_data.Pressure !== "undefined" ? station.dashboard_data.Pressure : "N.N.";
+                            msg.payload.compact.pressureTrend = station.dashboard_data.pressure_trend !== "undefined" ? station.dashboard_data.pressure_trend : "N.N.";
                         }
                         else {
                             msg.payload.compact.temperature = "N.N.";
@@ -73,13 +73,13 @@ module.exports = function (RED) {
                             if (module.type === 'NAModule1') { //Outdoor Sensor
 
                                 msg.payload.compact.outdoor.reachable = module.reachable || "false";
-                                msg.payload.compact.outdoor.battery_percent = module.battery_percent || "N.N.";
-                                msg.payload.compact.outdoor.rf_status = module.rf_status || "N.N.";
+                                msg.payload.compact.outdoor.battery_percent = module.battery_percent !== "undefined" ? module.battery_percent : "N.N.";
+                                msg.payload.compact.outdoor.rf_status = module.rf_status !== "undefined" ? module.rf_status : "N.N.";
 
                                 if(typeof module.dashboard_data !== "undefined") {
-                                    msg.payload.compact.outdoor.temperature = module.dashboard_data.Temperature || "N.N.";
-                                    msg.payload.compact.outdoor.humidity = module.dashboard_data.Humidity || "N.N.";
-                                    msg.payload.compact.outdoor.temperatureTrend = station.dashboard_data.temp_trend || "N.N.";
+                                    msg.payload.compact.outdoor.temperature = module.dashboard_data.Temperature !== "undefined" ? module.dashboard_data.Temperature : "N.N.";
+                                    msg.payload.compact.outdoor.humidity = module.dashboard_data.Humidity !== "undefined" ? module.dashboard_data.Humidity : "N.N.";
+                                    msg.payload.compact.outdoor.temperatureTrend = station.dashboard_data.temp_trend !== "undefined" ? station.dashboard_data.temp_trend : "N.N.";
                                 }
                                 else {
                                     msg.payload.compact.outdoor.temperature =  "N.N.";
@@ -91,13 +91,15 @@ module.exports = function (RED) {
                             if(module.type === 'NAModule3') { //Rain Sensor
 
                                 msg.payload.compact.rain.reachable = module.reachable || "false";
-                                msg.payload.compact.rain.battery_percent = module.battery_percent || "N.N.";
-                                msg.payload.compact.rain.rf_status = module.rf_status || "N.N.";
+                                msg.payload.compact.rain.battery_percent = module.battery_percent !== "undefined" ? module.battery_percent : "N.N.";
+                                msg.payload.compact.rain.rf_status = module.rf_status !== "undefined" ? module.rf_status : "N.N.";
 
                                 if(typeof module.dashboard_data !== "undefined") {
-                                    msg.payload.compact.rain.rain = module.dashboard_data.Rain || "N.N.";
-                                    msg.payload.compact.rain.sum_rain_24 = module.dashboard_data.sum_rain_24 || "N.N.";
-                                    msg.payload.compact.rain.sum_rain_1 = module.dashboard_data.sum_rain_1 || "N.N.";
+
+
+                                    msg.payload.compact.rain.rain = module.dashboard_data.Rain !== "undefined" ? module.dashboard_data.Rain : "N.N.";
+                                    msg.payload.compact.rain.sum_rain_24 = module.dashboard_data.sum_rain_24 !== "undefined" ? module.dashboard_data.sum_rain_24 :  "N.N.";
+                                    msg.payload.compact.rain.sum_rain_1 = module.dashboard_data.sum_rain_1 !== "undefined" ?  module.dashboard_data.sum_rain_1 : "N.N.";
                                 }
                                 else {
                                     msg.payload.compact.rain.rain =  "N.N.";
@@ -113,19 +115,19 @@ module.exports = function (RED) {
                                 tmpObj.name = module.module_name|| "N.N.";
                                 tmpObj.data_type = module.data_type || "N.N.";
 
-                                tmpObj.battery_percent = module.battery_percent || "N.N.";
-                                tmpObj.rf_status = module.rf_status || "N.N.";
+                                tmpObj.battery_percent = module.battery_percent !== "undefined" ? module.battery_percent : "N.N.";
+                                tmpObj.rf_status = module.rf_status !== "undefined" ? module.rf_status : "N.N.";
                                 tmpObj.reachable = module.reachable || "false";
 
                                 if(typeof module.dashboard_data !== "undefined") {
                                     tmpObj.dashboard_data = module.dashboard_data || "N.N.";
-                                    tmpObj.temperature = module.dashboard_data.Temperature  || "N.N.";
-                                    tmpObj.Humidity = module.dashboard_data.Humidity  || "N.N.";
-                                    tmpObj.CO2 = module.dashboard_data.CO2  || "N.N.";
-                                    tmpObj.min_temp = module.dashboard_data.min_temp  || "N.N.";
-                                    tmpObj.max_temp = module.dashboard_data.max_temp  || "N.N.";
-                                    tmpObj.date_min_temp = module.dashboard_data.date_min_temp  || "N.N.";
-                                    tmpObj.date_max_temp = module.dashboard_data.date_max_temp  || "N.N.";
+                                    tmpObj.temperature = module.dashboard_data.Temperature !== "undefined" ? module.dashboard_data.Temperature : "N.N.";
+                                    tmpObj.Humidity = module.dashboard_data.Humidity !== "undefined"  ? module.dashboard_data.Humidity : "N.N.";
+                                    tmpObj.CO2 = module.dashboard_data.CO2  !== "undefined" ? module.dashboard_data.CO2 : "N.N.";
+                                    tmpObj.min_temp = module.dashboard_data.min_temp !== "undefined" ? module.dashboard_data.min_temp : "N.N.";
+                                    tmpObj.max_temp = module.dashboard_data.max_temp !== "undefined" ? module.dashboard_data.max_temp  : "N.N.";
+                                    tmpObj.date_min_temp = module.dashboard_data.date_min_temp !== "undefined" ? module.dashboard_data.date_min_temp : "N.N.";
+                                    tmpObj.date_max_temp = module.dashboard_data.date_max_temp  !== "undefined" ? module.dashboard_data.date_max_temp : "N.N.";
                                 }
                                 else {
                                     tmpObj.dashboard_data =  "N.N.";
